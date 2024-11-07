926e32e0a96f94cb12197c851bc6fef28cdd12fa
Omar Guerra Alfaro <102171656+omGuerraAlfaro@users.noreply.github.com>
2024-11-07 16:14:48 -0300

    ok

--- src/app/components/modal-boletas-mantenedor/modal-boletas-mantenedor.component.ts
+++ src/app/components/modal-boletas-mantenedor/modal-boletas-mantenedor.component.ts
@@ -226,9 +226,28 @@ export class ModalBoletasMantenedorComponent implements OnInit {
       }));
 
       console.log("datosMatriculaPrueba", contratosPorEstudiante);
-
-      1
-
+      for (const contrato of contratosPorEstudiante) {
+        await new Promise<void>((resolve, reject) => {
+          this.pdfService.getPdfContrato(contrato).subscribe({
+            next: (blob) => {
+              const url = window.URL.createObjectURL(blob);
+              const link = document.createElement('a');
+              link.href = url;
+              link.download = `contrato_${contrato.estudiantes[0].rut}-${contrato.estudiantes[0].dv}.pdf`;
+              document.body.appendChild(link);
+              link.click();
+              document.body.removeChild(link);
+        
+              setTimeout(() => window.open(url, '_blank'), 100);
+              resolve();
+            },
+            error: (error) => {
+              console.error('Error al generar el PDF', error);
+              reject(error);
+            }
+          });
+        });
+      }
 
     } catch (error) {
       console.error('Error al generar el contrato', error);
